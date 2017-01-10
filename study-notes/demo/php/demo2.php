5dfc74bb2b995b75bc702aae84e465167c6fba30
dgf <990510638@qq.com>
2017-01-11 01:22:03 +0800

    add files

--- study-notes/demo/php/demo2.php
+++ study-notes/demo/php/demo2.php
@@ -13,18 +13,37 @@
 <body>
    	<div class="wrap">
         <?php
+            header("content-type:text/html;charset=utf-8");
             $servername = "localhost";
-            $username = "username";
-            $password = "password";
+            $username = "root";
+            $password = "";
+            $dbname = "first";
              
             // 创建连接
-            $conn = new mysqli($servername, $username, $password);
+            $conn = new mysqli($servername, $username, $password, $dbname);
              
             // 检测连接
             if ($conn->connect_error) {
                 die("连接失败: " . $conn->connect_error);
             } 
             echo "连接成功";
+
+            // // 使用 sql 创建数据表
+            // $sql = "CREATE TABLE MyGuests (
+            //     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+            //     firstname VARCHAR(30) NOT NULL,
+            //     lastname VARCHAR(30) NOT NULL,
+            //     email VARCHAR(50),
+            //     reg_date TIMESTAMP
+            // )";
+
+            // if ($conn->query($sql) === TRUE) {
+            //     echo "Table MyGuests created successfully";
+            // } else {
+            //     echo "创建数据表错误: " . $conn->error;
+            // }
+
+            // $conn->close();
         ?>
    	</div>
    
